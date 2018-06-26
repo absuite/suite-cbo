29059c651c7538535796fb5667b962bf113ec93d
suite <amiysd@163.com>
2018-06-27 00:00:39 +0800

    init

--- database/migrations/0000_00_00_000100_create_suite_cbo_biz_type_enum.php
+++ database/migrations/0000_00_00_000100_create_suite_cbo_biz_type_enum.php
@@ -13,8 +13,8 @@ class CreateSuiteCboBizTypeEnum extends Migration {
 	public function up() {
 		$md = Metadata::create($this->mdID);
 		$md->mdEnum('suite.cbo.biz.type.enum')->comment('业务类型');
-		$md->string('ship')->comment('销售发货')->default(0);
-		$md->string('rcv')->comment('采购收货')->default(1);
+		$md->string('ship')->comment('出货')->default(0);
+		$md->string('rcv')->comment('收货')->default(1);
 		$md->string('miscRcv')->comment('库存杂收')->default(2);
 		$md->string('miscShip')->comment('库存杂发')->default(3);
 		$md->string('transfer')->comment('库存调拨')->default(4);
@@ -25,6 +25,9 @@ class CreateSuiteCboBizTypeEnum extends Migration {
 		$md->string('payment')->comment('付款')->default(4);
 		$md->string('ar')->comment('应收')->default(4);
 		$md->string('ap')->comment('应付')->default(4);
+		$md->string('sale_settle')->comment('委托代销结算清单')->default(4);
+		$md->string('sale_ship')->comment('销售出货')->default(4);
+
 		$md->string('plan')->comment('计划预算')->default(4);
 		$md->string('expense')->comment('费用报销')->default(4);
 		$md->string('voucher')->comment('收支凭证')->default(4);
