cc7796e077be373561ed7b2923c3731347f5be85
suite <amiysd@163.com>
2018-07-09 20:50:44 +0800

    init

--- database/migrations/0000_00_00_000100_create_suite_cbo_biz_type_enum.php
+++ database/migrations/0000_00_00_000100_create_suite_cbo_biz_type_enum.php
@@ -18,19 +18,21 @@ class CreateSuiteCboBizTypeEnum extends Migration {
 		$md->string('miscRcv')->comment('库存杂收')->default(2);
 		$md->string('miscShip')->comment('库存杂发')->default(3);
 		$md->string('transfer')->comment('库存调拨')->default(4);
-		$md->string('moRcv')->comment('生产入库')->default(4);
-		$md->string('moIssue')->comment('生产领料')->default(4);
-		$md->string('process')->comment('工序转移')->default(4);
-		$md->string('receivables')->comment('收款')->default(4);
-		$md->string('payment')->comment('付款')->default(4);
-		$md->string('ar')->comment('应收')->default(4);
-		$md->string('ap')->comment('应付')->default(4);
-		$md->string('sale_settle')->comment('委托代销结算清单')->default(4);
-		$md->string('sale_ship')->comment('销售出货')->default(4);
+		$md->string('moRcv')->comment('生产入库')->default(5);
+		$md->string('moIssue')->comment('生产领料')->default(6);
+		$md->string('process')->comment('工序转移')->default(7);
+		$md->string('receivables')->comment('收款')->default(8);
+		$md->string('payment')->comment('付款')->default(9);
+		$md->string('ar')->comment('应收')->default(10);
+		$md->string('ap')->comment('应付')->default(11);
+		$md->string('sale_settle')->comment('委托代销结算清单')->default(12);
+		$md->string('sale_ship')->comment('销售出货')->default(13);
 
-		$md->string('plan')->comment('计划预算')->default(4);
-		$md->string('expense')->comment('费用报销')->default(4);
-		$md->string('voucher')->comment('收支凭证')->default(4);
+		$md->string('plan')->comment('计划预算')->default(14);
+		$md->string('expense')->comment('费用报销')->default(15);
+		$md->string('voucher')->comment('收支凭证')->default(16);
+		$md->string('waste')->comment('废品')->default(17);
+		$md->string('reject')->comment('不良品')->default(18);
 		$md->build();
 
 	}
